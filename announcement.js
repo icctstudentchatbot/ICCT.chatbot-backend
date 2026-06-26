7c9bf59ce370746f7f106d4c49def5092a0fa229
https <159163468+icctstudentchatbot@users.noreply.github.com>
2026-06-26 00:44:56 -1200

    Remove announcement schema and model definition

--- announcement.js
+++ announcement.js
@@ -1,12 +1,1 @@
-import mongoose from "mongoose";
 
-const announcementSchema = new mongoose.Schema({
-  title: String,
-  content: { type: String, required: true },
-  category: String,
-  date: String,
-  hash: { type: String, unique: true },
-  images: [String],
-});
-
-export default mongoose.model("Announcement", announcementSchema);
